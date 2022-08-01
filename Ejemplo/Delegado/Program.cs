d380679751fdfe3a7701d271a0c433b0c9bd3f83
String03 <40239185+String03@users.noreply.github.com>
2022-08-01 13:41:54 -0300

    Add files via upload

---
+++ Ejemplo/Delegado/Program.cs
@@ -0,0 +1,40 @@
+﻿using Operacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            int result = 0;
+
+            Division op = new Division();
+            op.MessageHandler = ShowMessage;
+
+            Console.WriteLine("Ingrese el operador 1");
+            a = Console.ReadLine();
+
+
+            Console.WriteLine("Ingrese el operador 2");
+            b = Console.ReadLine();
+
+            result = op.Divide(Convert.ToInt32(a), Convert.ToInt32(b));
+            Console.WriteLine($"Resultado: {result}");
+
+            Console.ReadKey();
+
+        }
+
+
+        public static void ShowMessage(int a, int b)
+        {
+            Console.WriteLine($"División entre cero, {a} - {b}");
+        }
+    }
+}
